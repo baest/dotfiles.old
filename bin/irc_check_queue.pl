43c5777b4263c6cd47f81a5692a53ac0681e9eea
Martin Frausing <martin.z.frausing@teliacompany.com>
2020-03-04 12:31:06 +0100

    Use another notifier

--- bin/irc_check_queue.pl
+++ bin/irc_check_queue.pl
@@ -1,3 +1,5 @@
+#!/usr/bin/env perl
+
 use 5.020;
 use Beanstalk::Client;
 use String::ShellQuote;
@@ -8,9 +10,15 @@ while(1) {
     my $job = $client->reserve;
     my %data = $job->args;
 
-    last if system(shell_quote('terminal-notifier',
-        '-message', $data{message},
-        '-title', $data{channel}));
+    #    last if (system(shell_quote('terminal-notifier',
+    #        '-message', $data{message},
+    #        '-title', $data{channel})));
+
+    $data{message} =~ s/"/\\"/g;
+    $data{channel} =~ s/"/\\"/g;
+    last if system(shell_quote('osascript', '-e', qq!display notification "$data{message}" with title "$data{channel}"!));
+
+    sleep 5;
 
     $job->delete;
 }
