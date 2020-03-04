f4f2948cfca89568297a4a9776bce18fb0acde26
Martin Frausing <martin.z.frausing@teliacompany.com>
2020-03-04 12:30:05 +0100

    ignore .ssh/config* files

--- checkout-repo.sh
+++ checkout-repo.sh
@@ -16,7 +16,11 @@ cfg config status.showUntrackedFiles no
 
 git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
 mkdir .zsh_history
-#rm .ssh/config*
+
+rm .ssh/config*
+echo .ssh/config >> .cfg/info/exclude
+echo .ssh/config.mac >> .cfg/info/exclude
+cfg update-index --assume-unchanged .ssh/config .ssh/config.mac
 
 #curl -Lks https://raw.githubusercontent.com/baest/dotfiles/master/checkout-repo.sh | zsh
 #cfg fetch --all; cfg reset --hard ; cfg rebase FETCH_HEAD; rm .ssh/config*
