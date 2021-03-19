a6c32b9871b40572e8d6da66e857a8ec5aeb4851
Martin Frausing <martin.z.frausing@teliacompany.com>
2021-03-19 20:12:35 +0100

    only load autojump when available

--- .zgen_plugins.zsh
+++ .zgen_plugins.zsh
@@ -1,6 +1,7 @@
 zgen load git@github.com:junegunn/fzf.git shell/completion.zsh
 zgen load git@github.com:junegunn/fzf.git shell/key-bindings.zsh
+#zgen load git@github.com:marlonrichert/zsh-autocomplete.git zsh-autocomplete.plugin.zsh
 zgen load git@github.com:tarruda/zsh-autosuggestions zsh-autosuggestions.zsh
 zgen load git@github.com:b4b4r07/enhancd init.sh
 zgen load git@github.com:zdharma/zsh-diff-so-fancy
-zgen oh-my-zsh plugins/autojump
+which autojump 2>/dev/null && zgen oh-my-zsh plugins/autojump
