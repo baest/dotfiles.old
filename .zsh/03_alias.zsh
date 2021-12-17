eeb8e850fd76eedc0930aca90ca652b1c0e18005
Martin Frausing <martin.z.frausing@teliacompany.com>
2021-12-17 11:33:16 +0100

    Only add these aliases when binarys are installed

--- .zsh/03_alias.zsh
+++ .zsh/03_alias.zsh
@@ -1,8 +1,14 @@
 alias ls="ls -F"
 #alias ll="ls -alh"
-alias ll="exa -laag --git"
-#alias ll="exa -laag --git --grid"
-alias tree="exa --tree -laag --git"
+
+if [[ -z "$(which exa)" ]]; then
+	alias ll="exa -laag --git"
+	#alias ll="exa -laag --git --grid"
+	alias tree="exa --tree -laag --git"
+else
+	alias ll="ls -alh"
+	alias tree="tree -a"
+fi
 
 alias cls="echo -ne '\033c'"
 
@@ -10,7 +16,6 @@ alias dc=cd
 #alias fd=/usr/bin/fdfind
 alias zudo="sudo zsh"
 alias top="top -c"
-alias tree="tree -a"
 alias 1='cd ..'
 alias 2='cd ../..'
 alias 3='cd ../../..'
@@ -24,8 +29,13 @@ alias tigb="tig blame"
 alias cfg="$REAL_GIT --git-dir=$HOME/.cfg/ --work-tree=$HOME"
 
 # nvim
-alias nv=nvim
-alias nvs='nvim -On'
-alias nv2='nvim -O2'
-alias damnit='nvim $(git grep -l "<<<< HEAD")'
+if [[ -z "$(which nvim)" ]]; then
+	alias nv=nvim
+	alias nvs='nvim -On'
+	alias nv2='nvim -O2'
+	alias damnit='nvim $(git grep -l "<<<< HEAD")'
+else
+	alias nv=vim
+	alias damnit='vim $(git grep -l "<<<< HEAD")'
+fi
 alias pbi=perlbrew_init
