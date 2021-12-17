922caa0b69a2f6e10a11bfa7f460922068eb6607
Martin Frausing <martin.z.frausing@teliacompany.com>
2021-12-17 06:31:10 +0100

    change to http

--- checkout-repo.sh
+++ checkout-repo.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-git clone --bare git@github.com:baest/dotfiles.git $HOME/.cfg
+git clone --bare https://github.com/baest/dotfiles.git $HOME/.cfg
 function cfg {
    /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
 }
